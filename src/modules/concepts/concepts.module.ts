7efc529d033232d6f83f4b13213264cb3a946c87
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2025-08-26 22:39:43 -0300

    removed concepts module

--- src/modules/concepts/concepts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConceptsController } from './concepts.controller';
-import { ConceptsService } from './concepts.service';
-
-@Module({
-  providers: [ConceptsService],
-  controllers: [ConceptsController],
-})
-export class ConceptsModule {}
