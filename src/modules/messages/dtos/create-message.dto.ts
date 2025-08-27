f701fed6364e1edfba0fa21e2e73bad32cbb3dba
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2025-08-26 22:01:33 -0300

    global pipes

--- src/modules/messages/dtos/create-message.dto.ts
+++ src/modules/messages/dtos/create-message.dto.ts
@@ -1,21 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateMessageDto {
   @ApiProperty({
     description: 'The content of the message',
     example: 'Hello, world!',
   })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly content: string;
 
   @ApiProperty({
     description: 'The sender of the message',
     example: 'User1',
   })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(50)
   readonly from: string;
 
   @ApiProperty({
     description: 'The recipient of the message',
     example: 'User2',
   })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(50)
   readonly to: string;
 }
